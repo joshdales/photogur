1a352ce8773a1f458ee436fad34f5fade7195fb3
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-10-10 16:14:20 -0400

    Create new GET and PATCH routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,10 @@ Rails.application.routes.draw do
   post 'pictures' => 'pictures#create'
   get 'pictures/new' => 'pictures#new'
 
-  get'pictures/:id' => 'pictures#show'
+  get 'pictures/:id' => 'pictures#show'
+
+  get 'pictures/:id/edit' => "pictures#edit"
+  patch 'pictures/:id' => "pictures#update"
 
 
 
