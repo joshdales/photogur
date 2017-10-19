a8e510c82be6feec3c4cacceda3d9450beef996b
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-10-19 16:01:04 -0400

    Define create method in Sessions Controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_url, notice:"Logged in!"
+    else
+      render :new
+    end
   end
 
   def destory
